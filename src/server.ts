d95e468f585f57f89743005efed948fe8c44d8ed
emonhasan17 <emon1.798045@gmail.com>
2025-12-24 15:56:03 +0600

    Updated file

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg"
 import dotenv from "dotenv"
 import path from "path"
 
-dotenv.config({path: path.join(process.cwd(), ".env")});
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 const app = express()
 const port = 5000
@@ -23,7 +23,7 @@ const initDb = async () => {
         CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
-        email VARCHAR(100) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
         age INT,
         phone VARCHAR(15),
         address TEXT,
@@ -53,12 +53,34 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello next level developer')
 })
 
-app.post('/', (req: Request, res: Response) => {
-    console.log(req.body);
+app.post('/users', async (req: Request, res: Response) => {
+
+    const { name, email } = req.body
+
+    try {
+
+        const result = await pool.query(`INSERT INTO users(name,email) VALUES($1,$2)
+             RETURNING *`, [name, email])
+        // console.log(result.rows[0]);
+        res.status(201).json({
+            success: true,
+            message: "data inserted successfully",
+            data: result.rows[0]
+        })
+
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+
+    }
+
     res.status(201).json({
         success: true,
         message: "api is working",
     })
+
 })
 
 app.listen(port, () => {
